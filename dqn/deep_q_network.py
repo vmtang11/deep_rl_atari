0ec7a2c92ea0275ec11b5da167d8c00fd8683a31
VishaalVenkatesh <venkateshvishaal@gmail.com>
2020-10-14 00:15:49 -0400

    Update deep_q_network.py

--- dqn/deep_q_network.py
+++ dqn/deep_q_network.py
@@ -11,11 +11,12 @@ class DeepQNetwork(nn.Module):
         self.checkpoint_dir = chkpt_dir
         self.checkpoint_file = os.path.join(self.checkpoint_dir, name)
         
-        self.conv1 = nn.Conv2d(input_dims[0], 32, 8, stride = 4)
+        self.conv1 = nn.Conv2d(input_dims[0], 32, 8, stride = 4)  # 32 filters, 8X8 kernel and stride = 4 pixels
+                                                                  # input_dims[0] = 4
         self.conv2 = nn.Conv2d(32, 64, 4, stride = 2)
         self.conv3 = nn.Conv2d(64, 64, 3, stride = 1)
         
-        fc_input_dims = self.calculate_conv_output_dims(input_dims)
+        fc_input_dims = self.calculate_conv_output_dims(input_dims)  # function to calculate input_dims to fc layer
         
         self.fc1 = nn.Linear(fc_input_dims, 512)
         self.fc2 = nn.Linear(512, n_actions)
@@ -30,15 +31,15 @@ class DeepQNetwork(nn.Module):
         dims = self.conv1(state)
         dims = self.conv2(dims)
         dims = self.conv3(dims)
-        return int(np.prod(dims.size()))
+        return int(np.prod(dims.size()))  # returns dim of output conv layer. This is to generalise to any input feature size and not hard code
     
     def forward(self, state):
-        conv1 = F.relu(self.conv1(state))
+        conv1 = F.relu(self.conv1(state))  # pass state through first conv layer and activate using ReLU
         conv2 = F.relu(self.conv2(conv1))
         conv3 = F.relu(self.conv3(conv2))
-        # conv3 shape is BS x n_filters x H x W
+        # conv3 shape is BS x n_filters x H x W (height and width of final convolved img)
         # need to reshape for fc layers to batch size and flatten
-        conv_state = conv3.view(conv3.size()[0], -1)
+        conv_state = conv3.view(conv3.size()[0], -1)  # equivalent to np.reshape(). The -1 tells the func to flatten all other dim
         flat1 = F.relu(self.fc1(conv_state))
         actions = self.fc2(flat1)
         
